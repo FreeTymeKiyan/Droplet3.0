740c9885f5a1185163df23a5e649644402d78bde
Kiyan <FreeTymeSunKiyan@gmail.com>
2012-11-11 22:37:23 +0800

    add a renrenhelper

---
+++ src/org/dianmobile/droplet/apis/RenrenHelper.java
@@ -0,0 +1,13 @@
+package org.dianmobile.droplet.apis;
+
+/**
+ * 人人相关的API类
+ * 封装所有人人相关的方法
+ * 如认证、发送状态、分享、@等
+ * 
+ * @author FreeTymeKiyan
+ * @version 0.0.1
+ */
+public class RenrenHelper {
+	
+}
