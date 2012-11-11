39f2dc8f8429ce1873cc949c62d912f9ca291ae4
Kiyan <FreeTymeSunKiyan@gmail.com>
2012-11-11 22:34:30 +0800

    add an adapter for main viewflow

---
+++ src/org/dianmobile/droplet/adapters/MainActivityAdapter.java
@@ -0,0 +1,54 @@
+package org.dianmobile.droplet.adapters;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+/**
+ * 主界面ViewFlow的适配器
+ * 需要适配不同的界面到主界面当中
+ * 主要是一个习惯界面，一个添加界面
+ * 
+ * @author FreeTymeKiyan
+ * @version 0.0.1
+ */
+public class MainActivityAdapter extends BaseAdapter {
+
+	@Override
+	public int getItemViewType(int position) {
+		// TODO Auto-generated method stub
+		return super.getItemViewType(position);
+	}
+	
+	@Override
+	public int getViewTypeCount() {
+		// TODO Auto-generated method stub
+		return super.getViewTypeCount();
+	}
+	
+	@Override
+	public int getCount() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Object getItem(int position) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public long getItemId(int position) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+
+}
